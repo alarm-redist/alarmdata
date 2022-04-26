badc265bb589b31db958368cc2a6c606fa21c024
Michael Zhao <46555283+mzhao80@users.noreply.github.com>
2022-04-26 10:48:39 -0400

    Added conditional logic based on existence of plan statistics

--- R/alarm_add_plan.R
+++ R/alarm_add_plan.R
@@ -2,8 +2,11 @@ alarm_add_plan <- function(plans, ref_plan, name = NULL) {
 
     if("comp_polsby" %in% colnames(plans)) {
         # redist_plans object already has summary statistics, so we must calculate them for ref_plan as well
-        ref_plan_stats <- calc_plan_stats(ref_plan)
+        # TODO: Create a redist_plans object, ref_redist_plan, from the integer vector ref_plan
+        ref_plan_stats <- calc_plan_stats(ref_redist_plan)
+        redist:::rbind.redist_plans(plans, ref_plan_stats)
+    } else {
+        redist::add_reference(plans, ref_plan_stats)
     }
-    redist::add_reference(plans, ref_plan_stats)
 
 }
