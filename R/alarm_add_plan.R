b8e0b303513d4c3d82d49b77e7678ce20b79308f
Michael Zhao <46555283+mzhao80@users.noreply.github.com>
2022-04-28 00:15:08 -0400

    Finished writing up function.

--- R/alarm_add_plan.R
+++ R/alarm_add_plan.R
@@ -1,12 +1,15 @@
-alarm_add_plan <- function(plans, ref_plan, name = NULL) {
+alarm_add_plan <- function(ref_plan, plans, map = NULL, name = NULL) {
 
     if("comp_polsby" %in% colnames(plans)) {
-        # redist_plans object already has summary statistics, so we must calculate them for ref_plan as well
-        # TODO: Create a redist_plans object, ref_redist_plan, from the integer vector ref_plan
-        ref_plan_stats <- calc_plan_stats(ref_redist_plan)
+        # redist_plans object already has summary statistics, so they must be calculated for ref_plan as well
+        if(is.null(map)) {
+            # Without a redist_map, summary statistics cannot be calculated
+            stop("A redist_map object must be passed in as an argument for 'map' in order to calculate summary statistics for the provided reference map.")
+        }
+        ref_plan_stats <- calc_plan_stats(ref_redist_plan, map)
         redist:::rbind.redist_plans(plans, ref_plan_stats)
     } else {
-        redist::add_reference(plans, ref_plan_stats)
+        redist::add_reference(plans, ref_plan_stats, name)
     }
 
 }
