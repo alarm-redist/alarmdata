41917b3bc6c88a92b173e3420ad9affe9a967733
Michael Zhao <46555283+mzhao80@users.noreply.github.com>
2022-04-30 10:44:24 -0400

    Finalize draft function and documentation.

--- R/alarm_add_plan.R
+++ R/alarm_add_plan.R
@@ -1,15 +1,20 @@
-alarm_add_plan <- function(ref_plan, plans, map = NULL, name = NULL) {
+#' This function facilitates comparing an existing (i.e., non-simulated) redistricting plan to a set of simulated plans.
+#'
+#' @param ref_plan An integer vector containing the reference plan. It will be renumbered to \code{1..ndists}
+#' @param plans A \code{redist_plans} object.
+#' @param map A \code{redist_map} object.
+#' @param name A human-readable name for the reference plan. Defaults to the name of \code{ref_plan}.
+#' @return A modified \code{redist_plans} object containing the reference plan, including calculated summary statistics for each plan.
+alarm_add_plan <- function(ref_plan, plans, map, name = NULL) {
 
     if("comp_polsby" %in% colnames(plans)) {
         # redist_plans object already has summary statistics, so they must be calculated for ref_plan as well
-        if(is.null(map)) {
-            # Without a redist_map, summary statistics cannot be calculated
-            stop("A redist_map object must be passed in as an argument for 'map' in order to calculate summary statistics for the provided reference map.")
-        }
+        ref_redist_plan <- redist::redist_plans(ref_plan, map, algorithm = "Reference", wgt = NULL)
         ref_plan_stats <- calc_plan_stats(ref_redist_plan, map)
         redist:::rbind.redist_plans(plans, ref_plan_stats)
     } else {
-        redist::add_reference(plans, ref_plan_stats, name)
+        new_redist_plans <- redist::add_reference(plans, ref_plan, name)
+        calc_plan_stats(new_redist_plans, map)
     }
 
 }
