29e0b904e38632eea8241ad351cc06758d6155db
Michael Zhao <46555283+mzhao80@users.noreply.github.com>
2022-05-16 14:04:15 -0700

    Add support for plan names. Fix rbind issue.

--- R/add_plan.R
+++ R/add_plan.R
@@ -12,16 +12,40 @@
 #' @export
 alarm_add_plan <- function(ref_plan, plans, map = NULL, calc_polsby = FALSE, name = NULL) {
     # redist_plans object already has summary statistics, so they must be calculated for ref_plan as well
+    if (!inherits(plans, "redist_plans"))
+        cli_abort("{.arg plans} must be a {.cls redist_plans}")
+    if (isTRUE(attr(plans, "partial")))
+        cli_abort("Reference plans not supported for partial plans objects")
+    if (!is.numeric(ref_plan))
+        cli_abort("{.arg ref_plan} must be numeric")
+    if (length(ref_plan) != nrow(redist::get_plans_matrix(plans)))
+        cli_abort("{.arg ref_plan} must have the same number of precincts as {.arg plans}")
+    if (is.null(name)) {
+        ref_str = deparse(substitute(ref_plan))
+        if (stringr::str_detect(ref_str, stringr::fixed("$")))
+            name = strsplit(ref_str, "$", fixed = TRUE)[[1]][2]
+        else name = ref_str
+    }
+    else {
+        if (!is.character(name))
+            cli_abort("{.arg name} must be a {.cls chr}")
+    }
+    if (name %in% levels(plans$draw))
+        cli_abort("Reference plan name already exists")
+
     if ("comp_polsby" %in% names(plans)) {
         if (is.null(map)) {
             cli_abort("{.arg map} must be a {.cls redist_map} in order to
                       calculate summary statistics for the provided reference plan.")
         }
+
         ref_redist_plan <- redist::redist_plans(redist::redist.sink.plan(ref_plan),
                                                 map, algorithm=attr(plans, "algorithm"))
-        print(ref_redist_plan)
         ref_plan_stats <- calc_plan_stats(ref_redist_plan, map, calc_polsby)
-        print(ref_plan_stats)
+        ref_plan_stats$draw <- name
+        attr(ref_plan_stats, "resampled") <- attr(plans, "resampled")
+        attr(ref_plan_stats, "compactness") <- attr(plans, "compactness")
+        attr(ref_plan_stats, "constraints") <- attr(plans, "constraints")
         rbind(ref_plan_stats, plans)
     } else { # just add reference
         redist::add_reference(plans, ref_plan, name)
