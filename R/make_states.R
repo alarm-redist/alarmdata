550db90908e31f98d86ca03d816f157b06a13480
Tyler Simko <tylersimko@gmail.com>
2022-05-18 18:20:24 +0000

    integrate calc_plan_stats to single states

--- R/make_states.R
+++ R/make_states.R
@@ -1,13 +1,23 @@
 make_state_map_one <- function(state, geometry = TRUE, epsg = alarm_epsg(state)) {
 
-    nd <- geomander::get_alarm(state = "ND", geometry = geometry, epsg = epsg)
+    nd <- geomander::get_alarm(state = state, geometry = geometry, epsg = epsg)
 
     nd <- nd %>%
-        summarise(across(starts_with("pop"), sum),
-                  across(starts_with("vap"), sum),
-                  across(matches("_(dem|rep)_"), sum),
-                  across(matches("^a[dr]v_"), sum),
+        summarise(dplyr::across(tidyselect::starts_with("pop"), sum),
+                  dplyr::across(tidyselect::starts_with("vap"), sum),
+                  dplyr::across(tidyselect::matches("_(dem|rep)_"), sum),
+                  dplyr::across(tidyselect::matches("^a[dr]v_"), sum),
                   geometry = sf::st_union(geometry))
+
+    id_nrv <- stringr::str_detect(colnames(nd), "arv_")
+    id_ndv <- stringr::str_detect(colnames(nd), "adv_")
+
+    nd <- nd %>%
+        mutate(nrv = rowMeans(as.data.frame(nd)[,id_nrv]),
+               ndv = rowMeans(as.data.frame(nd)[,id_ndv]),
+               nrv = round(nrv, 1),
+               ndv = round(ndv, 1)) %>%
+        relocate(geometry, .after = ndv)
 
     redist::redist_map(nd, pop_col = pop, ndists = 1, pop_tol = 0.01) %>%
         suppressWarnings() # avoid adjacency warning
@@ -19,8 +29,8 @@ make_state_plans_one <- function(state) {
     m <- matrix(1, nrow = 1, ncol = 5000)
     map <- make_state_map_one(state)
 
-    redist:::new_redist_plans(m, map, algorithm = "Single", wgt = NULL)
+    pl <- redist:::new_redist_plans(m, map, algorithm = "Single", wgt = NULL)
 
-    # TODO: Add summary stats once calc_plan_stats() done.
+    pl <- calc_plan_stats(pl, map)
 
 }
