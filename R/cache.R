3d80489c88a084f05cbf35f161268c1709bd0845
Christopher Kenny <ctk44@cornell.edu>
2022-08-30 20:50:41 -0400

    add cache control fns

---
+++ R/cache.R
@@ -0,0 +1,54 @@
+#' Work with the the `alarmdata` cache
+#'
+#' Functions to inspect and clear the cache. If the cache is not enabled, uses a
+#' temporary directory.
+#'
+#' @returns For `tt_cache_size()`, tthe size in bytes, invisbly
+#'
+#' @examples
+#' alarm_cache_size()
+#'
+#' @concept other
+#' @export
+#' @rdname alarm_cache
+alarm_cache_size <- function() {
+    files <- list.files(alarm_download_path(), recursive = TRUE, full.names = TRUE)
+    x <- sum(vapply(files, file.size, numeric(1)))
+    class(x) <- "object_size"
+    message(format(x, unit = "auto"))
+    invisible(as.numeric(x))
+}
+
+#' @param force FALSE by default. Asks the user to confirm if interactive. Does
+#' not clear cache if force is FALSE and not interactive.
+#' @returns For `alarm_cache_clear()`, the path to the cache, invisbly.
+#'
+#' @examples
+#' alarm_cache_clear()
+#'
+#' @export
+#' @rdname alarm_cache
+alarm_cache_clear <- function(force = FALSE) {
+    path <- alarm_download_path()
+    if (interactive() && !force) {
+        del <- utils::askYesNo(
+            msg = "Are you sure? All recursive directories will be deleted.",
+            default = FALSE
+        )
+    } else {
+        del <- force
+    }
+    if (del) unlink(path, recursive = TRUE)
+    invisible(path)
+}
+
+#' @returns For `alarm_cache_path()`, the path to the cache
+#'
+#' @examples
+#' alarm_cache_path()
+#'
+#' @export
+#' @rdname alarm_cache
+alarm_cache_path <- function() {
+    alarm_download_path()
+}
